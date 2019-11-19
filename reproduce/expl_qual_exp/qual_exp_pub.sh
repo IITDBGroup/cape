cc04b6d18a52d809bbfb8993a4c7954614d992d3
Boris Glavic (as perm@debussy) <lordpretzel@gmail.com>
2019-11-19 16:22:33 -0600

    typo

--- reproduce/expl_qual_exp/qual_exp_pub.sh
+++ reproduce/expl_qual_exp/qual_exp_pub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-capexplain -u antiprov -d antiprov -p antiprov -P 5436 --ptable dev.pub_large_no_domain --qtable pub_large_no_domain --ufile ./input/input_pub.txt
+capexplain explain -u antiprov -d antiprov -p antiprov -P 5436 --ptable dev.pub_large_no_domain --qtable pub_large_no_domain --ufile ./input/dblp.txt
