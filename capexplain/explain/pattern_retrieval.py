7bdbc1700b285aac7a8af21244716575fee019cc
Zhengjie Miao <zm42@duke.edu>
2019-11-30 02:17:52 -0800

    lazy

--- capexplain/explain/pattern_retrieval.py
+++ capexplain/explain/pattern_retrieval.py
@@ -107,10 +107,10 @@ def load_patterns(cur, pat_table_name, query_table_name, theta_thres=0.1, lambda
             continue
         if 'id' in pat[0] or 'id' in pat[1]:
             continue
-        if 'year' in pat[0]:
-            continue
-        if 'name' in pat[1] or 'venue' in pat[1]:
-            continue
+        # if 'year' in pat[0]:
+        #     continue
+        # if 'name' in pat[1] or 'venue' in pat[1]:
+        #     continue
         if 'primary_type' in pat[1] or 'description' in pat[1] or 'location_description' in pat[1] or 'community_area' in pat[1] or 'beat' in pat[1]:
             continue
 
