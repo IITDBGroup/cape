7d42421e347c46db9f0a6964fa6e2310064d4278
Boris Glavic <lordpretzel@gmail.com>
2019-12-02 14:58:10 -0600

    docker compose

--- docker/cape/build-image.sh
+++ docker/cape/build-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build -f ./Dockerfile -t iitdbgroup/2019-sigmod-reproducibility-cape .
+sudo docker build -f ./Dockerfile -t iitdbgroup/2019-sigmod-reproducibility-cape:latest .
