37568b1230aa78b8b4ae295234d85970978da7d9
Boris Glavic (as perm@debussy) <lordpretzel@gmail.com>
2019-12-02 17:46:48 -0600

    updated scripts

--- reproduce/expl_param_exp/params_exp.sh
+++ reproduce/expl_param_exp/params_exp.sh
@@ -2,5 +2,5 @@
 
 echo 'Running experiments for Figure 7'
 
-python3 expl_gt.py --ufile ./input/user_question_expl_gt_7.txt
+python3 params_exp.py --ufile ./input/user_question.csv
 python3 plot_params.py
