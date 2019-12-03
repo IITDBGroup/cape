fdb739257fa8e137fc0643f38d1607b3c5fac448
Zhengjie Miao <zm42@duke.edu>
2019-12-02 19:29:19 -0800

    updated scripts

--- reproduce/explanation.sh
+++ reproduce/explanation.sh
@@ -87,4 +87,4 @@ done
 
 
 echo 'Running experiments for Figure 7'
-python3 ./expl_param_exp/params_exp.py -h ${pgip} -P ${port} --ufile ./expl_param_exp/input/user_question.txt --rtfile ./experiments/expl_params_top_10_delta_5.txt
+python3 ./expl_param_exp/params_exp.py -h ${pgip} -P ${port} --ufile ./expl_param_exp/input/user_question.csv --rtfile ./experiments/expl_params_top_10_delta_5.txt
